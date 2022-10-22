922405926209e9271a6d0e9d9b9170a7e3a00009
Stefano Carmignola <stefano.crmg@gmail.com>
2022-10-22 10:16:21 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,35 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+/* 
+    Promises by default return Promise<any>.
+    How can we be sure that what we are fetching from the server adheres to the data schema we expect?
+
+    Using only TS we have the following choices:
+    1. write a type for the response object and take a leap of faith that we are doing it right
+    2. write a small typeguard and validate the response object against it
+  */
+
+const getDittoImage = async () =>
+  fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+    .then((_) => _.json())
+    .catch((e) => console.error(`Errors while fetching: `, e))
+
+const PokemonComponent: React.FC<{ imageUrl: string }> = ({ imageUrl }) => (
+  <div className="pokemonImage__container">
+    <img className="pokemonImage__content" src={imageUrl} />
+    <div className="pokemonNames">
+      <p className="highlight">en: Gengar</p>
+      <p className="highlight">jp: ゲンガー</p>
+    </div>
+  </div>
+)
 function App() {
   const [count, setCount] = useState(0)
+  const [imageUrl, setImageUrl] = useState('')
 
   useEffect(() => {
-    const _fetchingStuff = async () => {
-      const res = fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-      res
-        .then((_) => _.json())
-        .then((json) => console.log('its a me a ditto ', json))
-    }
-    _fetchingStuff()
+    getDittoImage()
   }, [])
 
   return (
@@ -26,10 +44,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <PokemonComponent imageUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/94.png" />
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
